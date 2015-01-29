fe277d3a5f5e5c46bd5d4484f67152e5c3e07725
DANIEL BULLOCK <DANIEL.I.BULLOCK@gmail.com>
2015-01-28 21:47:27 -0700

    "added advanceTask method"

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -57,6 +57,23 @@ $(document).ready(function () {
         advanceTask(task);
     });
 
+    var advanceTask = function(task) {
+        var modified = task.innerText.trim()
+        for (var i = 0; i < listo.length; i++) {
+            if (listo[i].task === modified) {
+                if (listo[i].id === 'new') {
+                    listo[i].id = 'inProgress';
+                } else if (listo[i].id === 'inProgress') {
+                    listo[i].id = 'archived';
+                } else {
+                    listo.splice(i, 1);
+                }
+
+                break;
+            }
+        }
+        task.remove();
+    };
 
 
 });
